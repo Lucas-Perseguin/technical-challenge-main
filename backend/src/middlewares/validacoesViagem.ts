88f6a3adafbe151d311dd4ab7e14104b3bfe4d82
Lucas <lucasperseguin@gmail.com>
2025-04-15 23:09:00 -0300

    chore: ajustes nas validacoes de viagem

--- backend/src/middlewares/validacoesViagem.ts
+++ backend/src/middlewares/validacoesViagem.ts
@@ -1,23 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
 import moment from "moment";
 import Motorista from "../models/Motorista.js";
+import Veiculo from "../models/Veiculo.js";
 
 export async function validacoesGeraisViagem(
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ) {
-	const { motorista: motoristaId, dataPartida, previsaoChegada } = req.body;
-	const motorista = await Motorista.findById(motoristaId);
-	if (!motorista) {
-		res.status(404).json({ erro: "Motorista não encontrado" });
-		return;
-	}
+	const { dataPartida, previsaoChegada, motorista: motoristaId } = req.body;
 
 	if (moment(previsaoChegada).isSameOrBefore(dataPartida)) {
 		res.status(400).json({
 			erro: "A previsão de chegada não pode ser anterior ou igual à data de partida",
 		});
+		return;
+	}
+
+	const motorista = await Motorista.findById(motoristaId);
+	if (!motorista) {
+		res.status(404).json({ erro: "Motorista não encontrado" });
 		return;
 	}
 
@@ -36,7 +38,7 @@ export async function validacoesCriacaoViagem(
 	res: Response,
 	next: NextFunction,
 ) {
-	const { status, dataPartida } = req.body;
+	const { status, dataPartida, veiculo: veiculoId } = req.body;
 
 	if (status !== "Planejada") {
 		res
@@ -49,6 +51,12 @@ export async function validacoesCriacaoViagem(
 		res.status(400).json({
 			erro: "A data de partida não pode ser anterior ao momento atual",
 		});
+		return;
+	}
+
+	const veiculo = await Veiculo.findById(veiculoId);
+	if (!veiculo) {
+		res.status(404).json({ erro: "Veículo não encontrado" });
 		return;
 	}
 
