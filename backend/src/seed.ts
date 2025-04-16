0479c3f5c6a39388bdb31eb769da31d4e1dcbf0b
Lucas <lucasperseguin@gmail.com>
2025-04-15 23:43:50 -0300

    chore: limpar seed

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -1,35 +1,7 @@
 import "dotenv/config";
 import Bcrypt from "bcrypt";
 import { connectDb } from "./config/mongo.js";
-import Motorista from "./models/Motorista.js";
 import Usuario from "./models/Usuario.js";
-
-const motoristas = [
-	{
-		nome: "João Silva",
-		cpf: "919.708.988-52".replace(/[^0-9]/g, ""),
-		cnh: {
-			numero: "68405447700",
-			validade: new Date("2026-12-01"),
-		},
-	},
-	{
-		nome: "Maria Oliveira",
-		cpf: "316.017.798-01".replace(/[^0-9]/g, ""),
-		cnh: {
-			numero: "57319582121",
-			validade: new Date("2025-10-15"),
-		},
-	},
-	{
-		nome: "Carlos Mendes",
-		cpf: "657.434.818-81".replace(/[^0-9]/g, ""),
-		cnh: {
-			numero: "65927717074",
-			validade: new Date("2027-04-30"),
-		},
-	},
-];
 
 const usuario = {
 	nome: "Admin",
@@ -41,11 +13,6 @@ const usuario = {
 const seed = async () => {
 	try {
 		connectDb();
-
-		await Motorista.deleteMany();
-		console.log("Coleção Motoristas limpa");
-		const motoristasCriados = await Motorista.insertMany(motoristas);
-		console.log(`${motoristasCriados.length} motoristas inseridos`);
 
 		await Usuario.deleteMany();
 		console.log("Coleção Usuários limpa");
