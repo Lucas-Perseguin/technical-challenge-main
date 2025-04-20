03975348179a99f82365c0b7b1885eb8e9c29599
Lucas <lucasperseguin@gmail.com>
2025-04-20 00:39:47 -0300

    fix: configuração docker react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss(), tsConfigPaths()],
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [react(), tailwindcss(), tsConfigPaths()],
+		preview: {
+			port: 4173,
+			host: true,
+		},
+		define: {
+			VITE_API_URL: JSON.stringify(env.VITE_API_URL),
+		},
+	};
 });
